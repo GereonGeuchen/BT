95b8df2b4137ec672f8d8e670e1b554efccfeb9c
pf657206 <pf657206@login23-1.hpc.itc.rwth-aachen.de>
2025-06-15 12:38:31 +0200

    early switching points optimization

--- selector/scripts/optimization_slurm.py
+++ selector/scripts/optimization_slurm.py
@@ -5,7 +5,7 @@ import joblib
 from asf.selectors import PerformanceModel, tune_selector
 
 def tune_performance_model(budget: int):
-    data = pd.read_csv(f"../data/ela_for_training/ela_with_algorithm_precisions/A1_B{budget}_5D_ela_with_state.csv")
+    data = pd.read_csv(f"../data/ela_for_training/ela_with_algorithm_precisions_early_switching/A1_B{budget}_5D_ela_with_state.csv")
     features = data.iloc[:, 4:-6]
     targets = data.iloc[:, -6:]
     groups = data["iid"]
@@ -21,15 +21,15 @@ def tune_performance_model(budget: int):
         cv=5,
         runcount_limit=75,
         seed=42,
-        output_dir=f"./smac_output_performance_test/B{budget}_performance"
+        output_dir=f"./smac_output_performance_early/B{budget}_performance"
     )
-    # os.makedirs("algo_performance_models", exist_ok=True)
-    # joblib.dump(pipeline, f"algo_performance_models/model_B{budget}.pkl")
+    os.makedirs("algo_performance_models_early_switching", exist_ok=True)
+    joblib.dump(pipeline, f"algo_performance_models_early_switching/model_B{budget}.pkl")
 
 
 def tune_switching_model(budget: int):
-    data = pd.read_csv(f"../data/ela_for_training/ela_with_optimal_precisions_ahead/A1_B{budget}_5D_ela_with_state.csv")
-    number_of_predictions = (1000 - budget) // 50 + 1
+    data = pd.read_csv(f"../data/ela_for_training/ela_with_optimal_precisions_ahead_early_switching/A1_B{budget}_5D_ela_with_state.csv")
+    number_of_predictions = 19 + ( (96 - budget) // 8 ) + 1 
 
     features = data.iloc[:, 4:-number_of_predictions]
     targets = data.iloc[:, -number_of_predictions:]
@@ -46,10 +46,10 @@ def tune_switching_model(budget: int):
         cv=5,
         runcount_limit=75,
         seed=42,
-        output_dir=f"./smac_output_switching_test/B{budget}_switching"
+        output_dir=f"./smac_output_switching_early/B{budget}_switching"
     )
-    # os.makedirs("switching_prediction_models", exist_ok=True)
-    # joblib.dump(pipeline, f"switching_prediction_models/model_B{budget}.pkl")
+    os.makedirs("switching_prediction_models_early_switching", exist_ok=True)
+    joblib.dump(pipeline, f"switching_prediction_models_early_switching/model_B{budget}.pkl")
 
 
 if __name__ == "__main__":
